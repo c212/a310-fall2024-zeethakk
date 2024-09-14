547fa32689d2ce30afe9b13e5764e1e5a9a0cbc7
Zeel Thakkar <zeelthakkar25@gmail.com>
2024-09-14 00:03:49 -0400

    midterm-Q3 updated

--- midterm-Q3.py
+++ midterm-Q3.py
@@ -1,42 +1,12 @@
+letter = "a"
+words = [ "bc", "cb" ]
 
-# letter = "a"
-# words = [ "bc", "cb" ]
-
-# def ben(letter, words):
-#     result = []
-#     for word in words:
-#         line = []
-#         for index in range(len(word)+1):
-#             line += [ word[0:index] + letter + word[index:] ]
-#         result += line
-#     return result
-
-# a = ben(letter, words)
-# print( a ) # [ "abc", "bac", "bca", "acb", "cab", "cba" ]
-
-# def perm(word):
-#     if len(word) <= 1:
-#         return [ word ]
+# def fact(n):
+#     if n == 0 or n == 1:
+#         return 1
 #     else:
-#         return ben(word[0], perm(word[1:]))
+#         return n * fact(n - 1)
 
-# print(perm("hat"))
-# print(perm("race"))
-# print(len(perm("whatever")) == 40320)
-
-# assert sorted(perm("hat")) == sorted(['hat', 'aht', 'ath', 'hta', 'tha', 'tah']), "First assert fails."
-# assert len(perm("race")) == fact(len("race")), "Second assert fails."
-# assert len(perm("whatever")) == 40320, "Third assert failed."
-# print ("All assertions passed without a problem")
-
-# Function to calculate factorial
-def fact(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * fact(n - 1)
-
-# Function to insert a letter into all positions of a word list
 def ben(letter, words):
     result = []
     for word in words:
@@ -46,16 +16,51 @@ def ben(letter, words):
         result += line
     return result
 
-# Function to calculate all permutations of a given word
+a = ben(letter, words)
+print( a ) # [ "abc", "bac", "bca", "acb", "cab", "cba" ]
+
 def perm(word):
     if len(word) <= 1:
         return [ word ]
     else:
         return ben(word[0], perm(word[1:]))
 
-# Testing the perm function with the given cases
+print(perm("hat"))
+print(perm("race"))
+print(len(perm("whatever")) == 40320)
+
 assert sorted(perm("hat")) == sorted(['hat', 'aht', 'ath', 'hta', 'tha', 'tah']), "First assert fails."
 assert len(perm("race")) == fact(len("race")), "Second assert fails."
 assert len(perm("whatever")) == 40320, "Third assert failed."
+print ("All assertions passed without a problem")
 
-print("All assertions passed without a problem")
+# # Function to calculate factorial
+# def fact(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * fact(n - 1)
+
+# # Function to insert a letter into all positions of a word list
+# def ben(letter, words):
+#     result = []
+#     for word in words:
+#         line = []
+#         for index in range(len(word)+1):
+#             line += [ word[0:index] + letter + word[index:] ]
+#         result += line
+#     return result
+
+# # Function to calculate all permutations of a given word
+# def perm(word):
+#     if len(word) <= 1:
+#         return [ word ]
+#     else:
+#         return ben(word[0], perm(word[1:]))
+
+# # Testing the perm function with the given cases
+# assert sorted(perm("hat")) == sorted(['hat', 'aht', 'ath', 'hta', 'tha', 'tah']), "First assert fails."
+# assert len(perm("race")) == fact(len("race")), "Second assert fails."
+# assert len(perm("whatever")) == 40320, "Third assert failed."
+
+# print("All assertions passed without a problem")
